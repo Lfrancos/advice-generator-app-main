201ed66cddac2d575ab5cda2a4d03ef63720052d
Lorenzo Franco <lorfran14@hotmail.com>
2022-03-04 18:36:42 -0500

    added all the functionality that I wanted

--- js/script.js
+++ js/script.js
@@ -1,16 +1,22 @@
+// select the items that I need
 const button = document.querySelector('.circle');
 const id = document.querySelector('.id');
 const advise = document.querySelector('.advice');
 
+// conditional for the time the advise has nothing in it.
+if (!advise.value) {
+    getAdvice();
+}
+
+
 button.addEventListener('click', getAdvice)
 
+// function to get the data and add it to the html
 async function getAdvice() {
     const data = await fetch('	https://api.adviceslip.com/advice');
     const slip = await data.json();
+
     id.textContent = slip.slip.id;
     advise.textContent = slip.slip.advice;
 
-    // console.log(slip.slip.id);
 }
-
-// getAdvice();
